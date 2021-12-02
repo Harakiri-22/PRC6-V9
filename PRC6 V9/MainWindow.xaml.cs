989cc7f21a17a69348fcc46c736bd21812ff30bb
Hakiro <Hakiro@DESKTOP-SATU0SK>
2021-12-02 15:19:04 +0300

    Добавьте файлы проекта.

---
+++ PRC6 V9/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PRC6_V9
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Information_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Выполнил Мишин Д.А. ИСП-34", "О программе", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void ComparisonTwo_Click(object sender, RoutedEventArgs e)
+        {
+            Triad triadOne = new Triad
+            {
+                First = Convert.ToInt32(a1.Text),
+                Second = Convert.ToInt32(a2.Text),
+                Third = Convert.ToInt32(a3.Text)
+            };
+
+            Triad triadTwo = new Triad
+            {
+                First = Convert.ToInt32(aa1.Text),
+                Second = Convert.ToInt32(aa2.Text),
+                Third = Convert.ToInt32(aa3.Text)
+            };
+
+            bool result;
+            result = Triad.Compare(triadOne.First, triadOne.Second, triadOne.Third, triadTwo.First, triadTwo.Second, triadTwo.Third);
+
+            if (result == true)
+            {
+                MessageBox.Show("Триады равны");
+            }
+            else
+            {
+                MessageBox.Show("Триады неравны");
+            }
+        }
+
+        private void ComparisonThree_Click(object sender, RoutedEventArgs e)
+        {
+            Triad triadOne = new Triad
+            {
+                First = Convert.ToInt32(a1.Text),
+                Second = Convert.ToInt32(a2.Text),
+                Third = Convert.ToInt32(a3.Text)
+            };
+
+            Triad triadTwo = new Triad
+            {
+                First = Convert.ToInt32(aa1.Text),
+                Second = Convert.ToInt32(aa2.Text),
+                Third = Convert.ToInt32(aa3.Text)
+            };
+
+            Triad triadThree = new Triad
+            {
+                First = Convert.ToInt32(aaa1.Text),
+                Second = Convert.ToInt32(aaa2.Text),
+                Third = Convert.ToInt32(aaa3.Text)
+            };
+
+            Triad.Compare(triadOne.First, triadOne.Second, triadOne.Third, triadTwo.First, triadTwo.Second, triadTwo.Third, triadThree.First, triadThree.Second, triadThree.Third, out bool resultOne, out bool resultThree);
+
+            if (resultOne == true && resultThree == true)
+            {
+                MessageBox.Show("Все триады равны");
+            }
+
+            if (resultOne == true && resultThree == false)
+            {
+                MessageBox.Show("Триада №2 и Триада №3 не равны");
+            }
+
+            if (resultOne == false && resultThree == true)
+            {
+                MessageBox.Show("Триада №1 и Триада №3 не равны");
+            }
+
+            if (resultOne == false && resultThree == false)
+            {
+                MessageBox.Show("Все триады не равны");
+            }
+        }
+    }
+}
---
+++ PRC6 V9/Triad.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PRC6_V9
+{
+    class Triad
+    {
+        private int _first;
+        private int _second;
+        private int _third;
+
+        public int First
+        {
+            get => _first;
+
+            set
+            {
+                if (value % 2 == 1)
+                {
+                    _first = value;
+                }
+                else
+                {
+                    MessageBox.Show("Ошибка, введено четное число");
+                }
+            }
+        }
+
+        public int Second
+        {
+            get => _second;
+
+            set
+            {
+                if (value % 2 == 1)
+                {
+                    _second = value;
+                }
+                else
+                {
+                    MessageBox.Show("Ошибка, введено четное число");
+                }
+            }
+        }
+
+        public int Third
+        {
+            get => _third;
+
+            set
+            {
+                if (value % 2 == 1)
+                {
+                    _third = value;
+                }
+                else
+                {
+                    MessageBox.Show("Ошибка, введено четное число");
+                }
+            }
+        }
+
+        public static bool Compare(int one, int two, int three, int first, int second, int third)
+        {
+            bool result = one == first && two == second && three == third;
+            return result;
+        }
+
+        public static void Compare(int one, int two, int three, int first, int second, int third, int oneFirst, int twoSecond, int threeThird, out bool resultOne, out bool resultTwo)
+        {
+            resultOne = one == first && two == second && three == third;
+
+            resultTwo = first == oneFirst && second == twoSecond && third == threeThird;
+        }
+    }
+}
